29bcbbf1eaf7cab9571906b7e02996a0e979090f
Dev-roxy <dev.rohit.roxy@gmail.com>
2025-01-03 12:02:23 +0530

    fix: correct token handling in middleware for user authentication

--- middleware.js
+++ middleware.js
@@ -3,11 +3,11 @@ import { jwtVerify } from 'jose';
 
 export async function middleware(request) {
   const token = request.cookies.get('accessToken') || request.headers.get('Authorization')?.split(' ')[1];
-  const accessToken = token.value
+  
   if (!token) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-
+  const accessToken = token.value
   try {
     const { payload } = await jwtVerify(accessToken, new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET));
     request.user = payload;
