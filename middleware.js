3d2835973b9931636b957935dc4ca1dfe0ca8f67
Dev-roxy <dev.rohit.roxy@gmail.com>
2025-01-03 12:12:37 +0530

    fix: improve middleware to handle login route and CSS errors

--- middleware.js
+++ middleware.js
@@ -2,21 +2,36 @@ import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
 export async function middleware(request) {
+
+  // just to resolve css error
+  if (request.nextUrl.pathname.startsWith("/_next")) {
+    return NextResponse.next();
+    }
+
   const token = request.cookies.get('accessToken') || request.headers.get('Authorization')?.split(' ')[1];
-  
+  console.log(request.nextUrl)
   if (!token) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    if (request.nextUrl.pathname == '/login'){
+      return NextResponse.next()
+    }else{
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
   }
   const accessToken = token.value
   try {
     const { payload } = await jwtVerify(accessToken, new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET));
     request.user = payload;
   } catch (error) {
-     return NextResponse.redirect(new URL('/login', request.url));
+     if (request.nextUrl.pathname == '/login'){
+      return NextResponse.next()
+     }else{
+      return NextResponse.redirect(new URL('/login', request.url));
+     }
   }
 
   return NextResponse.next();
 }
 const protectedRoutes = ["/dashboard" ,"/dashboard/:any*"];
+
 
 
